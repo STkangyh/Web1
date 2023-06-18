58c9e96d4e83678803eedaf703b66868d81fea81
YH <yhkang2003@gmail.com>
2023-06-18 22:51:17 +0900

    LOGIN

--- momentum/app.js
+++ momentum/app.js
@@ -1,18 +1,25 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
+}
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
 }
-
-function handleLinkClick(event){
-    
+const saveUsername = localStorage.getItem("username");
+if (saveUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings();
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
