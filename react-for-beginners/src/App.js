936be60cc89bfc0ded06e6166d56238c3b62e2f4
YH <yhkang2003@gmail.com>
2023-07-29 00:57:23 +0900

    TodoList

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,36 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }
+  console.log(toDos);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1>My ToDos {(toDos.length)}</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Write what you wanna do" />
+        <button>Add to do</button>
+      </form>
+      <hr />
+        <ul>
+          {toDos.map((item, index)=>
+          <li key={item}
+            >{item}</li>)
+          }
+        </ul>
     </div>
   );
 }
